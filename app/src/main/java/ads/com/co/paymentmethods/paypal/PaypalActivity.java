bcaec1ac491e350a2b7965eb18910e5e5c5b0f27
Agostinhodossantos <agostinhodossantos27@gmail.com>
2020-09-22 09:12:46 +0200

    override onActivityResult(...)

--- app/src/main/java/ads/com/co/paymentmethods/paypal/PaypalActivity.java
+++ app/src/main/java/ads/com/co/paymentmethods/paypal/PaypalActivity.java
@@ -2,13 +2,22 @@ package ads.com.co.paymentmethods.paypal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 import ads.com.co.paymentmethods.R;
 
@@ -32,5 +41,35 @@ public class PaypalActivity extends AppCompatActivity {
         Intent serviceConfig = new Intent(this, PayPalService.class);
         serviceConfig.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
         startService(serviceConfig);
+
+        PayPalPayment payment = new PayPalPayment(new BigDecimal("5.65"),
+                "USD", "My Awesome Item", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent paymentConfig = new Intent(this, PaymentActivity.class);
+        paymentConfig.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        paymentConfig.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(paymentConfig, 0);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            PaymentConfirmation confirm = data.getParcelableExtra(
+                    PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) {
+                try {
+                    Log.i("sampleapp", confirm.toJSONObject().toString(4));
+// TODO: send 'confirm' to your server for verification
+                } catch (JSONException e) {
+                    Log.e("sampleapp", "no confirmation data: ", e);
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("sampleapp", "The user canceled.");
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("sampleapp", "Invalid payment / config set");
+
+        }
+
     }
 }
