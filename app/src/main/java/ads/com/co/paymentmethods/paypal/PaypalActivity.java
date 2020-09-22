4f35e0906f6970784487e26798735e5c007da51c
Agostinhodossantos <agostinhodossantos27@gmail.com>
2020-09-22 09:06:00 +0200

    Intent serviceConfig

--- app/src/main/java/ads/com/co/paymentmethods/paypal/PaypalActivity.java
+++ app/src/main/java/ads/com/co/paymentmethods/paypal/PaypalActivity.java
@@ -2,10 +2,13 @@ package ads.com.co.paymentmethods.paypal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalService;
 
 import ads.com.co.paymentmethods.R;
 
@@ -20,8 +23,14 @@ public class PaypalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paypal);
+
+        final Button button = (Button) findViewById(R.id.paypal_button);
+
     }
 
     public void beginPayment(View view) {
+        Intent serviceConfig = new Intent(this, PayPalService.class);
+        serviceConfig.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(serviceConfig);
     }
 }
